4a519bbb716a4bd946cac98b416f134c21626f78
Toff2408 <joshuaeugene92@yahoo.com>
2023-12-19 07:02:18 -0600

    settings.py and urls

--- bankproject/urls.py
+++ bankproject/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from bankproject.settings import STATIC_ROOT, STATIC_URL
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('transfer.urls'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
